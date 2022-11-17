a86cf3891b9eed49d82eed3b3edb6bf7f3fdb088
balamanikandan9232 <balamanikandan9232@gmail.com>
2022-11-17 14:23:01 +0530

    day28

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,8 @@
 export function Header() {
   return (
     <div className="Header">
-      <h1>Arthi Group's</h1>
-      <h4>women's world</h4>
+      <h1>BALA & CO</h1>
+      <h4>Fashion world</h4>
     </div>
   );
 }
